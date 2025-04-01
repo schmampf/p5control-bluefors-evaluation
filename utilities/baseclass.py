4119e7bda695c32892946b3885bfecac444cb197
schmampf <oliver.irtenkauf@uni-konstanz.de>
2025-04-01 16:11:12 +0200

    rewritten baseclass

--- utilities/baseclass.py
+++ utilities/baseclass.py
@@ -1,4 +1,30 @@
-"""Module of a base class that can save/load/show its own dictionary."""
+"""
+BaseClass Module
+
+This module provides a base class for managing data storage and retrieval.
+It allows saving, loading, and displaying its attributes as a dictionary.
+
+Features:
+- Save and load attributes using pickle.
+- Open an HDF5 file using a GUI.
+- Manage figure and data storage directories.
+- Logging support for debugging and tracking operations.
+
+Dependencies:
+- Python 3.10
+- OS, Sys, Logging, Pickle, Platform
+- Matplotlib
+- qtpy for GUI interactions
+
+Usage Example:
+    obj = BaseClass(name="MyBase")
+    obj.saveData("test_data.pickle")
+    obj.loadData("test_data.pickle")
+    print(obj.showData())
+
+Author: Oliver Irtenkauf
+Date: 2025-04-01
+"""
 
 import os
 import sys
@@ -6,13 +32,11 @@ import pickle
 import logging
 import platform
 from importlib import reload
-import matplotlib.pyplot as plt
-
 from qtpy.QtWidgets import QApplication
 from qtpy.QtGui import QIcon
-
 from utilities.hdf5view.mainwindow import MainWindow
 
+# Configure logging
 reload(logging)
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger(__name__)
@@ -20,53 +44,53 @@ logger = logging.getLogger(__name__)
 
 class BaseClass:
     """
-    Base Class has method to save/load/show available data
+    BaseClass provides methods to save, load, and display its attributes.
+    It also integrates HDF5 file visualization through a Qt-based GUI.
     """
 
-    def __init__(
-        self,
-        name="eva",
-    ):
+    def __init__(self, name="base"):
         """
-        Description
-        """
-        # initialize name
-        self._name = name
+        Initialize the BaseClass instance with default storage settings.
 
-        # initialize logger
+        Args:
+            name (str): The name of the instance.
+        """
+        self._base_name = name
+
+        # Determine file directory based on OS
         match platform.system():
             case "Darwin":
                 file_directory = "/Volumes/speedyboy/measurement data/"
             case "Linux":
                 file_directory = "/home/oliver/Documents/measurement_data/"
-            case default:
+            case _:
                 file_directory = ""
-                logger.warning(
-                    "(%s) needs a file directory under %s.",
-                    self._name,
-                    default,
-                )
+                logger.warning("(%s) needs a valid file directory.", self._base_name)
 
-        # where to save fiure/data and find the measurement file
+        # Define base configuration for file management
         self.base = {
             "title": "",
             "sub_folder": "",
             "data_folder": "data/",
             "figure_folder": "figures/",
-            "ignore_while_saving": ["_name"],
+            "ignore_while_saving": ["_base_name", "_base_plot_name"],
             "file_directory": file_directory,
             "file_folder": "",
             "file_name": "",
         }
-        logger.info("(%s) ... BaseClass initialized.", self._name)
+        logger.info("(%s) ... BaseClass initialized.", self._base_name)
 
     def showFile(self):
         """
-        Opens hdf5view GUI to look into hdf5 file.
+        Open the HDF5 file using a Qt-based GUI viewer.
         """
-        logger.info("(%s) showFile()", self._name)
+        logger.info("(%s) showFile()", self._base_name)
+        file_name = os.path.join(
+            self.file_directory,
+            self.file_folder,
+            self.file_name,
+        )
 
-        file_name = f"{self.base['file_directory']}{self.base['file_folder']}{self.base['file_name']}"
         app = QApplication(sys.argv)
         app.setOrganizationName("hdf5view")
         app.setApplicationName("hdf5view")
@@ -76,84 +100,60 @@ class BaseClass:
         window.open_file(file_name)
         app.exec_()
 
-        # file_name = file_name.replace(" ", "\ ")
-        # command = f"hdf5view -f {file_name}"
-        # os.system(command)
-
-    def saveData(
-        self,
-        title=None,
-    ):
-        """saveData()
-        - safes self.__dict__ to pickle
+    def saveData(self, title: str = ""):
         """
-        logger.info("(%s) saveData()", self._name)
+        Save the object's attributes to a pickle file.
 
-        # Handle Title
-        if title is None:
-            title = f"{self.base['title']}.pickle"
+        Args:
+            title (str, optional): The filename for the saved data. Defaults to base title.
+        """
+        logger.info("(%s) saveData()", self._base_name)
+        if not title:
+            title = f"{self.title}.pickle"
 
-        # Handle data folder
-        folder = os.path.join(
-            os.getcwd(), self.base["data_folder"], self.base["sub_folder"]
-        )
-        check = os.path.isdir(folder)
-        if not check and self.base["data_folder"] != "":
+        # Ensure data folder exists
+        folder = os.path.join(os.getcwd(), self.data_folder, self.sub_folder)
+        if not os.path.isdir(folder):
             os.makedirs(folder)
 
-        # Get Dictionary
-        data = {}
-        for key, value in self.__dict__.items():
-            if key not in self.base["ignore_while_saving"]:
-                data[key] = value
+        # Filter data for saving
+        data = {
+            k: v for k, v in self.__dict__.items() if k not in self.ignore_while_saving
+        }
 
-        # save data to pickle
-        name = os.path.join(
-            os.getcwd(), self.base["data_folder"], self.base["sub_folder"], title
-        )
-        with open(name, "wb") as file:
+        # Save data
+        filepath = os.path.join(folder, title)
+        with open(filepath, "wb") as file:
             pickle.dump(data, file)
 
-    def loadData(
-        self,
-        title=None,
-    ):
-        """loadData()
-        - loads self.__dict__ from pickle
+    def loadData(self, title: str = ""):
         """
-        logger.info("(%s) loadData()", self._name)
+        Load attributes from a previously saved pickle file.
 
-        # Handle Title
-        if title is None:
-            title = f"{self.base['title']}.pickle"
+        Args:
+            title (str, optional): The filename to load. Defaults to base title.
+        """
+        logger.info("(%s) loadData()", self._base_name)
+        if not title:
+            title = f"{self.title}.pickle"
 
-        # get data from pickle
-        name = os.path.join(
-            os.getcwd(), self.base["data_folder"], self.base["sub_folder"], title
-        )
-        with open(name, "rb") as file:
+        filepath = os.path.join(os.getcwd(), self.data_folder, self.sub_folder, title)
+        with open(filepath, "rb") as file:
             data = pickle.load(file)
 
-        # Save Data to self.
+        self.__dict__.update(data)
 
-        plt.ioff()
-        for key, value in data.items():
-            self.__dict__[key] = value
-
-    def showData(
-        self,
-    ):
-        """showData()
-        - shows self.__dict__
+    def showData(self):
         """
-        logger.info("(%s) showData()", self._name)
+        Display stored attributes excluding those marked for ignoring.
 
-        # Get Dictionary
-        data = {}
-        for key, value in self.__dict__.items():
-            if key not in self.base["ignore_while_saving"]:
-                data[key] = value
-        return data
+        Returns:
+            dict: The stored attributes.
+        """
+        logger.info("(%s) showData()", self._base_name)
+        return {
+            k: v for k, v in self.__dict__.items() if k not in self.ignore_while_saving
+        }
 
     @property
     def title(self):
@@ -164,7 +164,7 @@ class BaseClass:
     def title(self, title: str):
         """set title"""
         self.base["title"] = title
-        logger.info("(%s) title = %s", self._name, title)
+        logger.debug("(%s) title = %s", self._base_name, title)
 
     @property
     def sub_folder(self):
@@ -175,7 +175,7 @@ class BaseClass:
     def sub_folder(self, sub_folder: str):
         """set sub_folder"""
         self.base["sub_folder"] = sub_folder
-        logger.info("(%s) sub_folder = %s", self._name, sub_folder)
+        logger.debug("(%s) sub_folder = %s", self._base_name, sub_folder)
 
     @property
     def data_folder(self):
@@ -186,7 +186,7 @@ class BaseClass:
     def data_folder(self, data_folder: str):
         """set data_folder"""
         self.base["data_folder"] = data_folder
-        logger.info("(%s) data_folder = %s", self._name, data_folder)
+        logger.debug("(%s) data_folder = %s", self._base_name, data_folder)
 
     @property
     def figure_folder(self):
@@ -197,7 +197,7 @@ class BaseClass:
     def figure_folder(self, figure_folder: str):
         """set figure_folder"""
         self.base["figure_folder"] = figure_folder
-        logger.info("(%s) figure_folder = %s", self._name, figure_folder)
+        logger.debug("(%s) figure_folder = %s", self._base_name, figure_folder)
 
     @property
     def ignore_while_saving(self):
@@ -208,7 +208,9 @@ class BaseClass:
     def ignore_while_saving(self, ignore_while_saving: str):
         """set ignore_while_saving"""
         self.base["ignore_while_saving"] = ignore_while_saving
-        logger.info("(%s) ignore_while_saving = %s", self._name, ignore_while_saving)
+        logger.debug(
+            "(%s) ignore_while_saving = %s", self._base_name, ignore_while_saving
+        )
 
     @property
     def file_directory(self):
@@ -219,7 +221,7 @@ class BaseClass:
     def file_directory(self, file_directory: str):
         """set file_directory"""
         self.base["file_directory"] = file_directory
-        logger.info("(%s) file_directory = %s", self._name, file_directory)
+        logger.debug("(%s) file_directory = %s", self._base_name, file_directory)
 
     @property
     def file_folder(self):
@@ -230,7 +232,7 @@ class BaseClass:
     def file_folder(self, file_folder: str):
         """set file_folder"""
         self.base["file_folder"] = file_folder
-        logger.info("(%s) file_folder = %s", self._name, file_folder)
+        logger.debug("(%s) file_folder = %s", self._base_name, file_folder)
 
     @property
     def file_name(self):
@@ -241,4 +243,4 @@ class BaseClass:
     def file_name(self, file_name: str):
         """set file_name"""
         self.base["file_name"] = file_name
-        logger.info("(%s) file_name = %s", self._name, file_name)
+        logger.debug("(%s) file_name = %s", self._base_name, file_name)
