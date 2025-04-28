956fc11f224ca17a7ac49a4b36af9b0001e6b186
Robotop64 <capalbo.david@gmail.com>
2025-04-28 15:11:26 +0200

    add copy snippet

--- reorganize/utilities/snippets.py
+++ reorganize/utilities/snippets.py
@@ -21,3 +21,40 @@
 #     magnet=-0.0098989881,
 # )
 # ===================================================
+# Recursivly copy the contents of a h5py file
+# -------------------------------------------------
+# from h5py import File, Group
+
+# path = "/home/dacap/Downloads/OI-25c-09 2025-04-15 unbroken 0.hdf5"
+# new_path = "/home/dacap/Downloads/OI-25c-09 2025-04-15 unbroken 0.copy.hdf5"
+
+# # example data path: /measurement/Group/Subgroup/subsubgroup/dataset
+
+# file = File(path, "r")
+# dir = file.get("measurement")
+
+
+# # copy stuff
+# def copy_group(source: Group, dest: Group):
+#     """
+#     Recursively copy a group and its contents from source to destination.
+#     """
+#     for key, item in source.items():
+#         if isinstance(item, Group):
+#             new_group = dest.create_group(key)
+#             copy_group(item, new_group)
+#         else:
+#             try:
+#                 dest.copy(item, key)
+#             except Exception as e:
+#                 print(f"Error copying {key}: {e}")
+
+
+# # Create a new file to copy the data into
+# with File(new_path, "w") as new_file:
+#     # Create a new group in the new file
+#     new_group = new_file.create_group("measurement")
+
+#     # Copy the contents of the original group to the new group
+#     copy_group(dir, new_group)
+# ===================================================
