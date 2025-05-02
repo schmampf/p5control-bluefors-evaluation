a2029dabea46f94e0e8bd9e9a187521a01e24275
Robotop64 <capalbo.david@gmail.com>
2025-05-02 16:13:38 +0200

    attempt logging improvements
    goal: incremental tabbing and reseting on flagged posts

--- reorganize/utilities/logging.py
+++ reorganize/utilities/logging.py
@@ -7,8 +7,11 @@ from integration.files import DataCollection
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger(__name__)
 
-indent_level: int = 0
+glob_indent_lvl: int = 0
+local_indent_lvl: int = 0
+anchor: list[int] = []
 bib: DataCollection = DataCollection()
+
 
 class LogLevel(Enum):
     CRITICAL = logging.CRITICAL
@@ -18,43 +21,69 @@ class LogLevel(Enum):
     DEBUG = logging.DEBUG
     NOTSET = logging.NOTSET
 
+
 CRITICAL = LogLevel.CRITICAL
 ERROR = LogLevel.ERROR
 WARNING = LogLevel.WARNING
 INFO = LogLevel.INFO
-DEBUG = LogLevel.DEBUG 
-        
+DEBUG = LogLevel.DEBUG
+
+
 def setup(nbib: DataCollection) -> None:
     global bib
     bib = nbib
     print(INFO, START, "Logger.setup()")
-    
+
+
 def set_level(level: LogLevel) -> None:
     """Set the logging level."""
     logger.setLevel(level.value)
     print(INFO, START, "Logger.set_level()")
 
+
 class Indent(Enum):
     START = 0
     KEEP = 1
     END = 2
+    INC = 3
+    RESET = 4
+
 
 START = Indent.START
 KEEP = Indent.KEEP
 END = Indent.END
+INC = Indent.INC
+RESET = Indent.RESET
 
-def print(level: LogLevel, nextIndent: Indent=KEEP, msg: str = "") -> None:
+
+def print(plevel: LogLevel, nextIndent: Indent = KEEP, msg: str = "") -> None:
     """Print a message with the specified logging level and indentation."""
-    global indent_level
+    global glob_indent_lvl
+    global local_indent_lvl
+    global anchor
     global data
-    
+
+    # if cascade:
+    #     temp = last_anchor.pop() if last_anchor else 0
+    #     last_anchor.append(glob_indent_lvl)
+    #     glob_indent_lvl = temp
+
     if nextIndent == Indent.START:
-        indent_level = 0
-    
+        glob_indent_lvl = 0
+
+    # if nextIndent == Indent.INC:
+    #     glob_indent_lvl += 2
+
+    # if nextIndent == Indent.RESET:
+    #     glob_indent_lvl = last_anchor.pop() if last_anchor else 0
+
     name = bib.data.name if not bib.data.name == "default" else "..."
-    logger.log(level.value, "(%s) %s%s", name, " " * indent_level, msg)
-    
-    indent_level = 2
-    
+    logger.log(plevel.value, "(%s) %s%s", name, " " * glob_indent_lvl, msg)
+
+    # if plevel == LogLevel.DEBUG:
+    #     glob_indent_lvl += 2
+    # else:
+    glob_indent_lvl = 2
+
     if nextIndent == Indent.END:
-        indent_level = 0
+        glob_indent_lvl = 0
