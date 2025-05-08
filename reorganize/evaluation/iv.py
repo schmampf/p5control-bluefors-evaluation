finish single map plot and fix calculation

reorganize/evaluation/iv.py
@@ -52,7 +52,13 @@ class Axis:
     def bins(self):
         if len(self.values) == 0:
             return 0
-        return len(self.values) - 1
+        return len(self.values)
+
+    def copy(self) -> "Axis":
+        new_axis = Axis()
+        new_axis.name = self.name
+        new_axis.values = self.values.copy()
+        return new_axis
 
 
 @dataclass
@@ -71,7 +77,7 @@ class Curve:
         if name == "values":
             if len(value) != self.dependent_ax.bins:
                 raise ValueError(
-                    f"Values must have the same length as the number of bins ({self.dependent_ax.bins})."
+                    f"Values must have the same length as the number of bins. Is:({len(value)}) Req:({self.dependent_ax.bins})."
                 )
         super().__setattr__(name, value)
 
@@ -137,6 +143,15 @@ class Map:
             self.values = np.full(
                 (self.x_axis.bins, self.y_axis.bins), np.nan, dtype=np.float64
             )
+
+    def copy(self) -> "Map":
+        new_map = Map()
+        new_map.name = self.name
+        new_map.x_axis = self.x_axis.copy()
+        new_map.y_axis = self.y_axis.copy()
+        new_map.z_axis = self.z_axis.copy()
+        new_map.values = self.values.copy()
+        return new_map
 
 
 @dataclass
@@ -402,21 +417,23 @@ def process_curve_sets(
     )
     set = collection.evaluation.cached_sets
 
-    conductance_quantum = constants.physical_constants["conductance quantum"][0]
+    G_0 = constants.physical_constants["conductance quantum"][0]
 
-    diff_conductance = (
-        np.gradient(set["adwin"].curves["current"], set["adwin"].curves["voltage"])
-        / conductance_quantum
-    )
-    diff_resistance = np.gradient(
-        set["adwin"].curves["voltage"], set["adwin"].curves["current"]
-    )
-    temp = np.nanmean(set["bluefors"].curves["temperature"])
+    c_bins = set["adwin"].curves["current-bin"]
+    c_binned = set["adwin"].curves["current-voltage"]
+    v_bins = set["adwin"].curves["voltage-bin"]
+    v_binned = set["adwin"].curves["voltage-current"]
+
+    diff_resistance = np.gradient(v_binned, c_bins)
+    diff_conductance = np.gradient(c_binned, v_bins) / G_0
+
+    temps = set["bluefors"].curves["temperature"]
+    temp = np.nanmean(temps)
 
     diffs = DataSet()
     diffs.curves["diff_conductance"] = diff_conductance
     diffs.curves["diff_resistance"] = diff_resistance
-    diffs.curves["temperature"] = temp
+    diffs.curves["mean_temp"] = temp
 
     collection.evaluation.cached_sets["diffs"] = diffs
 

reorganize/plotting/plot.py
@@ -20,6 +20,7 @@ class StyleKeys(Enum):
     Y_LABEL = auto()
     Z_LABEL = auto()
 
+    SCALE = auto()
     X_LIM = auto()
     Y_LIM = auto()
     Z_LIM = auto()
@@ -35,6 +36,7 @@ class StyleKeys(Enum):
 X_LABEL = StyleKeys.X_LABEL
 Y_LABEL = StyleKeys.Y_LABEL
 Z_LABEL = StyleKeys.Z_LABEL
+SCALE = StyleKeys.SCALE
 X_LIM = StyleKeys.X_LIM
 Y_LIM = StyleKeys.Y_LIM
 Z_LIM = StyleKeys.Z_LIM
@@ -94,11 +96,26 @@ def map(bib: DataCollection, type: list[str], styling: list[dict[StyleKeys, Any]
 
     for i, t in enumerate(type):
         match t:
-            case "VVI":
+            case "VXI" | "IXV" | "dIXR" | "dVXC":
+                Logger.print(
+                    Logger.DEBUG,
+                    msg=f"Plotting map: {t}",
+                )
+
                 style = styling[i]
-                map = bib.result.maps["VVI"]
-                xcoords = map.x_axis.values
-                ycoords = map.y_axis.values
+                map_def = bib.result.maps[t]
+                xcoords_def = map_def.x_axis.values
+                ycoords_def = map_def.y_axis.values
+
+                xcoords = xcoords_def.copy()
+                ycoords = ycoords_def.copy()
+                map = map_def.copy()
+
+                scale = style.get(SCALE)
+                if scale is not None:
+                    xcoords = xcoords_def * scale[0]
+                    ycoords = ycoords_def * scale[1]
+                    map.values = map_def.values * scale[2]
 
                 plt.imshow(
                     map.values,
@@ -112,53 +129,30 @@ def map(bib: DataCollection, type: list[str], styling: list[dict[StyleKeys, Any]
                         ycoords[-1],
                     ),
                 )
-                # region labels
-                plt.xlabel(style.get(X_LABEL) or map.x_axis.name)
-                plt.ylabel(style.get(Y_LABEL) or map.y_axis.name)
-                if style.get(CBAR, False):
-                    plt.colorbar(label=style.get(Z_LABEL, map.z_axis.name))
-                # endregion
-                # region lims
-                if style.get(X_LIM):
-                    plt.xlim(style.get(X_LIM))
-                if style.get(Y_LIM):
-                    plt.ylim(style.get(Y_LIM))
-                if style.get(Z_LIM):
-                    plt.clim(style.get(Z_LIM))
-                # endregion
-                # region ticks
-                ticks = style.get(TICKS)
-                if ticks:
-                    plt.locator_params(axis="x", nbins=ticks[0])
-                    plt.locator_params(axis="y", nbins=ticks[1])
-                    # if style.get(CBAR, False):
-                    #     cbar = plt.colorbar(label=style.get(Z_LABEL, map.z_axis.name))
-                    #     cbar_ticks = style.get("CBAR_TICKS")  # Add a key for color bar ticks
-                    #     if cbar_ticks:
-                    #         cbar.set_ticks(np.linspace(cbar.vmin, cbar.vmax, cbar_ticks))
 
-                # endregion
+                style_map(map, style)
 
     plt.show()
 
 
-def get_unit(name: str) -> str:
-    """
-    Get the unit of a given name.
-    """
-    if name == "current":
-        return "A"
-    elif name == "voltage":
-        return "V"
-    elif name == "time":
-        return "s"
-    else:
-        return ""
-
-
-def unpack_style(
-    style: dict[str, str],
-):
-    plt.xlabel(style.get("x-axis", "err"))
-    plt.ylabel(style.get("y-axis", "err"))
-    plt.colorbar(label=style.get("z-axis", "err"))
+def style_map(map, style: dict[StyleKeys, Any]):
+    # region labels
+    plt.xlabel(style.get(X_LABEL) or map.x_axis.name)
+    plt.ylabel(style.get(Y_LABEL) or map.y_axis.name)
+    if style.get(CBAR, False):
+        plt.colorbar(label=style.get(Z_LABEL, map.z_axis.name))
+    # endregion
+    # region lims
+    if style.get(X_LIM):
+        plt.xlim(style.get(X_LIM))
+    if style.get(Y_LIM):
+        plt.ylim(style.get(Y_LIM))
+    if style.get(Z_LIM):
+        plt.clim(style.get(Z_LIM))
+    # endregion
+    # region ticks
+    ticks = style.get(TICKS)
+    if ticks:
+        plt.locator_params(axis="x", nbins=ticks[0])
+        plt.locator_params(axis="y", nbins=ticks[1])
+    # endregion

reorganize/utilities/macros.py
@@ -38,9 +38,13 @@ def bulk_eval(bib: DataCollection):
     result = {}
     num_slices = bib.params.available_measurement_entries
     slice_index = np.linspace(0, num_slices - 1, num_slices, dtype=int)
-    init: bool = False
 
-    VVI = np.empty((num_slices, 0), dtype=float)
+    VXI = np.empty((num_slices, bib.iv_params.bins), dtype=float)
+    IXV = np.empty((num_slices, bib.iv_params.bins), dtype=float)
+    dIXR = np.empty((num_slices, bib.iv_params.bins), dtype=float)
+    dVXC = np.empty((num_slices, bib.iv_params.bins), dtype=float)
+
+    TX = np.empty((num_slices, 1), dtype=float)
 
     Logger.suppress_print = True
     for i in slice_index:
@@ -55,34 +59,84 @@ def bulk_eval(bib: DataCollection):
         eval(bib, "adwin")
         eval(bib, "bluefors")
 
-        iv_x = bib.evaluation.cached_sets["adwin"].curves["voltage-bin"]
-        iv_z = bib.evaluation.cached_sets["adwin"].curves["current-voltage"]
+        t_b = bib.evaluation.cached_sets["adwin"].curves["time-bin"]
+        v_b = bib.evaluation.cached_sets["adwin"].curves["voltage-bin"]
+        i_b = bib.evaluation.cached_sets["adwin"].curves["current-bin"]
+        temp_b = bib.evaluation.cached_sets["bluefors"].curves["temperature"]
 
-        if not init:
-            VVI = np.empty((num_slices, iv_x.size), dtype=float)
-            init = True
+        iv = bib.evaluation.cached_sets["adwin"].curves["current-voltage"]
+        vi = bib.evaluation.cached_sets["adwin"].curves["voltage-current"]
+        d_res = bib.evaluation.cached_sets["diffs"].curves["diff_resistance"]
+        d_cond = bib.evaluation.cached_sets["diffs"].curves["diff_conductance"]
+        m_temp = bib.evaluation.cached_sets["diffs"].curves["mean_temp"]
 
-        VVI[i, :] = np.gradient(np.array(iv_z, dtype=float))
+        VXI[i, :] = np.array(iv, dtype=float)
+        IXV[i, :] = np.array(vi, dtype=float)
+        dIXR[i, :] = np.array(d_res, dtype=float)
+        dVXC[i, :] = np.array(d_cond, dtype=float)
+
+        TX[i, 0] = np.array(m_temp, dtype=float)
     Logger.suppress_print = False
 
-    iv_y_labels = [
+    y_labels = [
         GenEval.MeasurementHeader.parse_number(l)[0]
         for l in bib.params.available_measurement_entries_labels
     ]
-    result["VVI"] = IVEval.Map(
+
+    result["VXI"] = IVEval.Map(
         x_axis=IVEval.Axis(
-            name="Voltage (V)",
-            values=iv_x,
+            name=r"Voltage ($V$)",
+            values=v_b,
         ),
         y_axis=IVEval.Axis(
-            name="Voltage (V)",
-            values=np.array(iv_y_labels),
+            name="X",
+            values=np.array(y_labels),
         ),
-        z_axis=IVEval.Axis(
-            name="Current (I)",
-            values=iv_z,
+        z_axis=IVEval.Axis(name=r"Current ($I$)"),
+        values=VXI,
+    )
+    result["IXV"] = IVEval.Map(
+        x_axis=IVEval.Axis(
+            name=r"Current ($I$)",
+            values=i_b,
         ),
-        values=VVI,
+        y_axis=IVEval.Axis(
+            name="X",
+            values=np.array(y_labels),
+        ),
+        z_axis=IVEval.Axis(name=r"Voltage ($V$)"),
+        values=IXV,
+    )
+    result["dIXR"] = IVEval.Map(
+        x_axis=IVEval.Axis(
+            name=r"Current ($I$)",
+            values=i_b,
+        ),
+        y_axis=IVEval.Axis(
+            name="X",
+            values=np.array(y_labels),
+        ),
+        z_axis=IVEval.Axis(name=r"d$V$/d$I$ ($O$)"),
+        values=dIXR,
+    )
+    result["dVXC"] = IVEval.Map(
+        x_axis=IVEval.Axis(
+            name=r"Voltage (V)",
+            values=v_b,
+        ),
+        y_axis=IVEval.Axis(
+            name="X",
+            values=np.array(y_labels),
+        ),
+        z_axis=IVEval.Axis(name=r"d$I$/d$V$ ($G_0$)"),
+        values=dVXC,
     )
 
+    result["TX"] = IVEval.Curve(
+        dependent_ax=IVEval.Axis(
+            name="X",
+            values=np.array(y_labels),
+        ),
+        values=TX,
+    )
     bib.result.maps = result