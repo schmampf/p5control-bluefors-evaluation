e415ce123fd32049a5d05861c67353b69ef7334f
David Capalbo <capalbo.david@gmail.com>
2025-05-21 23:18:10 +0200

    continue Tien Gordon

--- simulation/src/TienGordon.jl
+++ simulation/src/TienGordon.jl
@@ -1,81 +1,87 @@
 module TienGordon
-using Bessels
+import SpecialFunctions as SF
 using JLD2
+using Interpolations
 export I
 
 #-constants-------------
-const e = 1.602e-19 # C
-const ħ = 1.055e-34 # Js
+const e = 1.602176634e-19 # C
+const ħ = 1.054571817e-34 # Js
 #-----------------------
 #-variables-------------
 ν = 1.0
 extend = false
+I0 = []
+V0 = []
+n = 200
 #-----------------------
 
 const data_dir = joinpath(@__DIR__, "..", "data")
 
 # find a value in y for a value in x
 # if no direct match is found extrapolate from the closest
-function findVal(exact::Real, x::Array{Float64}, y::Array{Float64})
+function findVal(query::Real, x::Array{Float64}, y::Array{Float64})
     for (i, val) in enumerate(x)
-        if val == exact
-            return y[i]
-        elseif val < exact
+        if val < query
             continue
-        elseif val > exact
+        elseif val == query
+            return y[i]
+        elseif val > query
             x_c, y_c = x[i], y[i]
 
-            if i < 2
+            if i <= 1
                 return y_c
-            else
-                return y[i-1]
             end
 
-            # x_p, y_p = x[i-1], y[i-1]
-            # x_diff = x_c - exact
-            # y_diff = y_c - y_p
-            # y_exact = y_p + (y_diff * (x_diff / (x_c - x_p)))
-            # return y_exact
+            x_p, y_p = x[i-1], y[i-1]
+            x_diff = x_c - query
+            y_diff = y_c - y_p
+            return y_p + (y_diff * (x_diff / (x_c - x_p)))
         end
     end
-    # println("Warning: requested $exact")
-    return y[end]
+
+    if query > x[end]
+        return y[end]
+    end
+
+    throw(ArgumentError("Query value $query not found in x array."))
 end
 
-function V0()
+function set_τ(τ::Real)
+    global V0, I0
     data = jldopen(data_dir * "/iv.jld2", "r")
-    dict = data["raw"]
-    key1 = first(keys(dict))
-    V0 = dict[key1][1]
+    v, i = data["normalized"][τ]
+    V0, I0 = v, i
     if extend
         V0 = vcat(reverse(V0) .* -1, V0)
+        I0 = vcat(reverse(I0) .* -1, I0)
     end
-    return V0
+    return τ
 end
 
-function IV₀(V₀::Real, Vω::Real, n::Int, τ::Real)
-    global ν
+function save_curve(τ::Real)
     data = jldopen(data_dir * "/iv.jld2", "r")
-    ref_V0, ref_I0 = data["normalized"][τ]
-    ω = 2 * π * ν
+    v, i = data["normalized"][τ]
 
-
-    # special case
-    if n == 0
-        b_arg = (e * Vω) / (ħ * ω)
-        bessel = (besselj(0, b_arg))^2
-        V_shift = V₀
-        I = findVal(V_shift, ref_V0, ref_I0)
-        return I * bessel
+    open(data_dir * "/iv_$τ.dat", "w") do file
+        for j in 1:lastindex(v)
+            println(file, v[j], "\t", i[j])
+        end
     end
+end
 
-    # default case
+function IV₀(V₀::Real, Vω::Real)
+    global ν, I0, V0, n
+
+    ω = 2 * π * ν
+    b_arg = (e * Vω) / (ħ * ω) / 10000
+
     out = 0.0
     for ni in -n:n
-        b_arg = (e * Vω) / (ħ * ω)
-        bessel = (besselj(ni, b_arg))^2
+        bessel = (SF.besselj(ni, b_arg))^2
         V_shift = V₀ - (ni * ħ * ω) / (e)
-        I = findVal(V_shift, ref_V0, ref_I0)
+        V_shift = abs(V_shift) # symmetry around the y axis
+        I = findVal(V_shift, V0, I0)
         out += I * bessel
     end
     return out
