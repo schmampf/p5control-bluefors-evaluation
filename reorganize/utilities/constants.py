5c3c0cfbdc3bede28e1e953eebf0bec8bb8f3bbe
David Capalbo <117840567+Robotop64@users.noreply.github.com>
2025-05-30 22:57:42 +0200

    add py constants

---
+++ reorganize/utilities/constants.py
@@ -0,0 +1,13 @@
+# region imports
+# std
+from typing import Final
+# 3rd party
+# local
+# endregion
+
+e: Final[float] = 1.602176634e-19  # Elementary charge in Coulombs
+h: Final[float] = 6.62607015e-34  # Planck's constant in Joule seconds
+pi: Final[float] = 3.141592653589793  # Pi constant
+hbar: Final[float] = h / (2 * pi)  # Reduced Planck's constant in Joule seconds
+G0: Final[float] = 7.748091729e-5  # Conductance quantum in Siemens
+delta: Final[float] = 189e-6  # Default delta value for numerical calculations
