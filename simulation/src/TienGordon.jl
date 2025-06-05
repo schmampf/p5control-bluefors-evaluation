dd5747aae7c902754c5a0b00bab1be3f848c8f3e
Robotop64 <capalbo.david@gmail.com>
2025-06-05 16:30:58 +0200

    fix PAMAR

--- simulation/src/TienGordon.jl
+++ simulation/src/TienGordon.jl
@@ -1,7 +1,6 @@
 module TienGordon
 import SpecialFunctions as SF
-using JLD2
-using Interpolations
+using JLD2, Interpolations, Match
 export I
 
 #-constants-------------
@@ -12,28 +11,82 @@ const ħ = h / (2 * π) # Js
 #-variables-------------
 ν = 1.0
 Δ = 1.0
-I0 = []
-V0 = []
+τ = 0.0
 n = 0
 m = 1.0
-itp = nothing
 reference = nothing
 #-----------------------
 
 const data_dir = joinpath(@__DIR__, "..", "data")
 
-function set_τ(τ::Real)
-    global V0, I0, itp
-    data = jldopen(reference[1], "r")
-    v, i = data[reference[2]][τ]
-    V0, I0 = v, i
+@enum ReferenceType single multiple
+struct ReferenceDesc
+    path::String
+    set::String
+end
+struct Reference
+    V0::Vector{<:Real}
+    I0::Vector{<:Real}
+    Im::Vector{Tuple{Vector{<:Real},Interpolations.Extrapolation}}
+end
+function load_reference(ref::ReferenceDesc; type::ReferenceType=single)
+    global τ, reference
+    @match type begin
+        $single => begin
+            data = jldopen(ref.path, "r")
+            v, i = data[ref.set][τ]
+            V0, I0 = v, i
+            V0 = vcat(-reverse(V0), V0)
+            I0 = vcat(-reverse(I0), I0)
+            itp = linear_interpolation(V0, I0, extrapolation_bc=Line())
+            reference = Reference(V0, I0, [(I0, itp)])
+        end
+        $multiple => begin
+            data = jldopen(ref.path, "r")
+            set = data[string(τ)]
+            V0 = set["V0"]
+            I0 = set["I0"]
+            Im = set["Im"]
 
-    V0 = vcat(-reverse(V0), V0)
-    I0 = vcat(-reverse(I0), I0)
+            Im = [Im[:, mi] for mi in axes(Im, 2)]
 
-    itp = linear_interpolation(V0, I0, extrapolation_bc=Line())
+            if V0[1] != -V0[end]
+                V0 = vcat(-reverse(V0), V0)
+                I0 = vcat(-reverse(I0), I0)
+                Im = [vcat(-reverse(Im[mi]), Im[mi]) for mi in 1:lastindex(Im)]
+            end
 
-    return τ
+            continuous(x) = begin
+                stepsize = x[2] - x[1]
+                state = true
+                idx = 1
+                while idx < lastindex(V0)
+                    if V0[idx] + stepsize == V0[idx+1]
+                        idx += 1
+                    else
+                        state = false
+                        break
+                    end
+                end
+                return state
+            end
+            fix(a, a_fix, b) = begin
+                lookup = Dict(zip(Real.(round.(a, digits=2)), b))
+                return [get(lookup, Real(round(x, digits=2)), 0) for x in a_fix]
+            end
+
+            if !continuous(V0)
+                V0_fix = collect(V0[1]:V0[2]-V0[1]:V0[end])
+                I0_fix = fix(V0, V0_fix, I0)
+                Im_fix = [fix(V0, V0_fix, Im[mi]) for mi in 1:lastindex(Im)]
+            end
+
+            reference = Reference(V0, I0, [
+                (Im_fix[mi], linear_interpolation(V0_fix, Im_fix[mi], extrapolation_bc=Line()))
+                for mi in 1:lastindex(Im)
+            ])
+        end
+    end
 end
 
 function save_curve(τ::Real)
@@ -48,17 +101,17 @@ function save_curve(τ::Real)
 end
 
 function IV₀(V₀::Real, Vω::Real)
-    global ν, I0, V0, n, m, Δ
+    global ν, n, m, Δ, reference
 
     ω = 2 * π * ν
     b_arg = (e * Vω) / (ħ * ω)
 
     out = 0.0
 
-    for ni in -n:n
-        bessel = (SF.besselj(ni, Vω))^2
-        V_shift = V₀ - (ni * ħ * ω) / (Δ * e)
-        I = itp(V_shift)
+    for mi in 1:m, ni in -n:n
+        bessel = (SF.besselj(ni, mi * Vω))^2
+        V_shift = V₀ - (ni * ħ * ω) / (mi * Δ * e)
+        I = reference.Im[mi][2](V_shift)
         out += I * bessel
     end
     return out
