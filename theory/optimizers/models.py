8facd096a67d4ed8ddb2e4d968df6f0c8c0e457c
schmampf <oliver.irtenkauf@uni-konstanz.de>
2025-12-08 11:35:40 +0100

    renaming, dynes -> bcs, tau -> G_N

--- theory/optimizers/models.py
+++ theory/optimizers/models.py
@@ -1,23 +1,29 @@
-from typing import Callable, Optional, TypeAlias
+"""
+document sting
+"""
+
+from typing import Optional, TypeAlias, Callable
 
 import jax
 import jax.numpy as jnp
 import numpy as np
-from jax import Array, jit, config
-from numpy.typing import NDArray, ArrayLike
+from jax import Array, jit
+from numpy.typing import NDArray
 
-from theory.models.PAT import get_I_pat_nA_from_I0_A0
+from theory.models.tg import get_I_pat_nA_from_I0_A0
 from theory.models.bcs_jnp import G_0_muS_jax, currents, thermal_energy_gap
 
 jax.config.update("jax_enable_x64", True)
 
 NDArray64: TypeAlias = NDArray[np.float64]
-ModelFunction: TypeAlias = Callable[..., ArrayLike]
+ModelFunction: TypeAlias = Callable[..., NDArray64]
 ModelType: TypeAlias = tuple[ModelFunction, NDArray[np.bool]]
 
 
-def models(
-    model: str = "dynes", E_mV: Optional[NDArray64] = None, N: Optional[int] = None
+def get_model(
+    model: str = "bcs",
+    E_mV: Optional[NDArray64] = None,
+    N: Optional[int] = None,
 ) -> ModelType:
 
     if E_mV is None:
@@ -30,10 +36,10 @@ def models(
     @jit
     def get_dynes_jnp(
         V_mV: Array,
-        tau: Array,
+        G_N: Array,
         T_K: Array,
         Delta_mV: Array,
-        Gamma_mV: Array,
+        gamma_meV: Array,
     ) -> Array:
         Delta_T_mV: Array = thermal_energy_gap(Delta_meV=Delta_mV, T_K=T_K)
         I_mV: Array = currents(
@@ -41,45 +47,43 @@ def models(
             E_meV=E_mV_jax,
             T_K=T_K,
             Delta_meV=Delta_T_mV,
-            Gamma_meV=Gamma_mV,
+            gamma_meV=gamma_meV,
         )
-        I_nA: Array = I_mV * tau * G_0_muS_jax
+        I_nA: Array = I_mV * G_N * G_0_muS_jax
         return I_nA
 
     match model:
-        case "dynes":
+        case "bcs" | "dynes":
 
-            def get_dynes(
+            def get_bcs(
                 V_mV: NDArray64,
-                tau: float,
+                G_N: float,
                 T_K: float,
                 Delta_mV: float,
-                Gamma_mV: float,
+                gamma_meV: float,
             ) -> NDArray64:
                 I_nA_jax: Array = get_dynes_jnp(
                     V_mV=jnp.array(V_mV, dtype=jnp.float64),
-                    tau=jnp.array(tau, dtype=jnp.float64),
+                    G_N=jnp.array(G_N, dtype=jnp.float64),
                     T_K=jnp.array(T_K, dtype=jnp.float64),
                     Delta_mV=jnp.array(Delta_mV, dtype=jnp.float64),
-                    Gamma_mV=jnp.array(Gamma_mV, dtype=jnp.float64),
+                    gamma_meV=jnp.array(gamma_meV, dtype=jnp.float64),
                 )
                 I_nA: NDArray64 = np.array(I_nA_jax, dtype=np.float64)
                 return I_nA
 
             # Mask which parameter are used
-            parameter_mask: NDArray[np.bool] = np.array(
-                [True, True, True, True, False, False]
-            )
-            return get_dynes, parameter_mask
+            parameter_mask: NDArray[np.bool] = np.full((4), True, dtype=bool)
+            return get_bcs, parameter_mask
 
-        case "dynes+pat":
+        case "pat" | "bcs+pat" | "dynes+pat":
 
-            def get_dynes_pat(
+            def get_bcs_pat(
                 V_mV: NDArray64,
-                tau: float,
+                G_N: float,
                 T_K: float,
                 Delta_mV: float,
-                Gamma_mV: float,
+                gamma_meV: float,
                 A_mV: float,
                 nu_GHz: float,
             ) -> NDArray64:
@@ -87,10 +91,10 @@ def models(
 
                 I_dynes_jax: Array = get_dynes_jnp(
                     V_mV=V_mV_jax,
-                    tau=jnp.array(tau, dtype=jnp.float64),
+                    G_N=jnp.array(G_N, dtype=jnp.float64),
                     T_K=jnp.array(T_K, dtype=jnp.float64),
                     Delta_mV=jnp.array(Delta_mV, dtype=jnp.float64),
-                    Gamma_mV=jnp.array(Gamma_mV, dtype=jnp.float64),
+                    gamma_meV=jnp.array(gamma_meV, dtype=jnp.float64),
                 )
                 I_dynes: NDArray64 = np.array(I_dynes_jax, dtype=np.float64)
                 I_dynes_pat: NDArray64 = get_I_pat_nA_from_I0_A0(
@@ -103,8 +107,8 @@ def models(
                 return I_dynes_pat
 
             # Mask which parameter are used
-            parameter_mask: NDArray[np.bool] = np.full((6), True, dtype=np.bool)
-            return get_dynes_pat, parameter_mask
+            parameter_mask: NDArray[np.bool] = np.full((6), True, dtype=bool)
+            return get_bcs_pat, parameter_mask
 
         case _:
             raise KeyError("model not found.")
